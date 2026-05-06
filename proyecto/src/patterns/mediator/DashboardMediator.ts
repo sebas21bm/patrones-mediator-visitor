8756bdbf291ec24a2e2b3377c0cd618db9ae4836
sebas <barmoraseb@gmail.com>
2026-05-06 12:41:23 -0600

    me faltó un parentesis.

--- proyecto/src/patterns/mediator/DashboardMediator.ts
+++ proyecto/src/patterns/mediator/DashboardMediator.ts
@@ -43,7 +43,7 @@ export class DashboardMediator implements Mediator{
             );
         }
         if (componente instanceof PanelVentas && evento === "actualizar"){
-            const metricas = await obtenerMetricasVentas
+            const metricas = await obtenerMetricasVentas();
             const alertas = await revisarAlertasVentas();
 
             localStorage.setItem(
